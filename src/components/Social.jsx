d83a84e3b0612bc71ecccd1d9fa193046089cc16
Adamh1990 <68036525+Adamh1990@users.noreply.github.com>
2023-08-04 19:44:19 -0500

    babel again

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -51,6 +51,7 @@ const Social = () => {
                 {links.map(({ id, child, href, style, download }) => (
                     <li 
                     key={id} 
+                    // eslint-disable-next-line no-useless-concat
                     className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style
                     }
                     >
